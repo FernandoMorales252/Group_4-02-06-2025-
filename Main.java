fd59f7f2e1dbebfee4e6772e210964a04d91229d
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-06-02 16:54:25 -0600

    Cambios en la clase Main

--- Main.java
+++ Main.java
@@ -3,5 +3,34 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
+
+    public void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Empleado> empleados = new ArrayList<>();
+
+        String continuar;
+
+        do {
+            System.out.println("Ingrese los datos del empleado:");
+
+            System.out.print("ID: ");
+            int id = scanner.nextInt();
+
+            scanner.nextLine(); // Limpiar buffer
+
+            System.out.print("Nombre: ");
+            String nombre = scanner.nextLine();
+
+            System.out.print("Apellido: ");
+            String apellido = scanner.nextLine();
+
+            System.out.print("Departamento: ");
+            String departamento = scanner.nextLine();
+
+
+
+
+    }
     
 }
